a6f9ef3f431b33f4ba164d3fafc0715433b12743
Anastasiia Yemelianova <49037481+discoDeaky@users.noreply.github.com>
2020-05-11 08:33:24 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
-'use strict';
+$(document).ready(function () {
+  $(".hero").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    speed: 2000,
+  });
+});
+
+("use strict");
 (function () {
-	function trackScroll() {
-		var scrolled = window.pageYOffset;
-		var coords = document.documentElement.clientHeight;
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
 
-		if (scrolled > coords) {
-			goTopBtn.classList.add('back_to_top-show');
-		}
-		if (scrolled < coords) {
-			goTopBtn.classList.remove('back_to_top-show');
-		}
-	}
+    if (scrolled > coords) {
+      goTopBtn.classList.add("back_to_top-show");
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove("back_to_top-show");
+    }
+  }
 
-	function backToTop() {
-		if (window.pageYOffset > 0) {
-			window.scrollBy(0, -80);
-			setTimeout(backToTop, 10);
-		}
-	}
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -50);
+      setTimeout(backToTop, 10);
+    }
+  }
 
-	var goTopBtn = document.querySelector('.back_to_top');
+  var goTopBtn = document.querySelector(".back_to_top");
 
-	window.addEventListener('scroll', trackScroll);
-	goTopBtn.addEventListener('click', backToTop);
+  window.addEventListener("scroll", trackScroll);
+  goTopBtn.addEventListener("click", backToTop);
 })();
